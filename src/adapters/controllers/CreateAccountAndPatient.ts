838e21983b61c7afd27e20be704d22f4aca37213
jaymeco <jayme.anunciacao@gmail.com>
2021-03-22 12:03:08 -0300

    Adiciona caso de uso de criação de pacientes no controller

--- src/adapters/controllers/CreateAccountAndPatient.ts
+++ src/adapters/controllers/CreateAccountAndPatient.ts
@@ -1,23 +1,31 @@
 import { Request, Response } from "express";
 import { CreateAccount } from "../../domain/UseCases/CreateAccont";
+import { CreatePatient } from "../../domain/UseCases/CreatePatient";
 
 export class CreateAccountAndPatient {
   private createAccountUseCase;
+  private createPatientUseCase;
 
-  constructor (createAccountUseCase: CreateAccount){
+  constructor (createAccountUseCase: CreateAccount, createPatientUseCase: CreatePatient){
     this.createAccountUseCase = createAccountUseCase;
+    this.createPatientUseCase = createPatientUseCase;
   }
 
   async handle(request: Request, response: Response): Promise<Response>{
     try {
-      const { email, password } = request.body;
+      const { email, password, ...rest } = request.body;
 
       const account = await this.createAccountUseCase.execute({
         email,
         password
       });
 
-      return response.status(201).json(account);
+      const patient = this.createPatientUseCase.execute({
+        account_id: account.id,
+        ...rest
+      });
+
+      return response.status(201).json({account, patient});
     } catch (error) {
       return response.status(400).json({
         error: error.message || 'Unexpected error!'
