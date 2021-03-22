36ad65ddccc29fa451c4bff95482c46691c64bc6
jaymeco <jayme.anunciacao@gmail.com>
2021-03-22 12:11:15 -0300

    Adiciona injeção de dependencia no controller de criação de paciente

--- src/domain/DTOs/CreatePatient.ts
+++ src/domain/DTOs/CreatePatient.ts
@@ -2,15 +2,5 @@ export interface ICreatePatientRequestDTO {
   account_id: string;
   first_name: string;
   last_name: string;
-  gender: string;
   birthday: string;
-  occupation: string;
-  state: string;
-  city: string;
-  weight: string;
-  height: string;
-  imc: number;
-  blood_type: string;
-  condition: string;
-  cpf: string;
 }
--- src/main/CreateAccountAndPatient.ts
+++ src/main/CreateAccountAndPatient.ts
@@ -1,8 +1,11 @@
 import { CreateAccount } from '../domain/UseCases/CreateAccont';
+import { CreatePatient } from '../domain/UseCases/CreatePatient';
 import { CreateAccountAndPatient } from '../adapters/controllers/CreateAccountAndPatient';
 
 const createAccountUseCase = new CreateAccount();
 
-const createAccountPatientController = new CreateAccountAndPatient(createAccountUseCase);
+const createPatientUseCase = new CreatePatient();
+
+const createAccountPatientController = new CreateAccountAndPatient(createAccountUseCase, createPatientUseCase);
 
 export { createAccountPatientController };
