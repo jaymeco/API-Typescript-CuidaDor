6748164be9f9f62df5eb0c38ed09f997a0c83bb4
jaymeco <jayme.anunciacao@gmail.com>
2021-03-23 15:50:00 -0300

    Adiciona entidade de profissionais

---
+++ src/domain/entities/Professionals.ts
@@ -0,0 +1,59 @@
+import { Column, OneToOne, JoinColumn, Entity, PrimaryColumn, OneToMany } from "typeorm";
+import { v4 as uuid } from 'uuid';
+import { Account } from "./Account";
+
+export class Professionals{
+
+  @PrimaryColumn()
+  public readonly id: string;
+
+  @OneToOne(()=> Account)
+  @JoinColumn({ name: 'account_id' })
+  public account: Account;
+
+  @Column()
+  public account_id: string;
+
+  @Column()
+  public first_name: string;
+
+  @Column()
+  public last_name: string;
+
+  @Column()
+  public gender: string;
+
+  @Column()
+  public birthday: string;
+
+  @Column()
+  public experience: string;
+
+  @Column()
+  public state: string;
+
+  @Column()
+  public city: string;
+
+  @Column()
+  public specialization: string;
+
+  @Column()
+  public about: string;
+
+  @Column()
+  public cpf: string;
+
+  constructor (){
+    if(!this.id){
+      this.id = uuid();
+    }
+    this.gender = 'Não informado';
+    this.specialization = 'Não informado';
+    this.state = 'Não informado';
+    this.about = 'Não informado';
+    this.experience = 'Não informado';
+    this.cpf = 'Não informado';
+    this.city = 'Não informado';
+  }
+}
