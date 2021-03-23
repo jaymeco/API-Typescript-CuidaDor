e936e2adeb9aa3f0a81f439c776f9eb4b7696561
jaymeco <jayme.anunciacao@gmail.com>
2021-03-23 19:49:06 -0300

    Adiciona injeção de dependecia na criação de conta e profissional

---
+++ src/main/CreateAccountAndProfessional.ts
@@ -0,0 +1,9 @@
+import { CreateProfessional } from '../domain/UseCases/CreateProfessional';
+import { CreateAccount } from '../domain/UseCases/CreateAccont';
+import { CreateAccountAndProfessional } from '../adapters/controllers/CreateAccountAndProfessional';
+
+const createAccountUseCase = new CreateAccount();
+
+const createProfessionalUseCase = new CreateProfessional();
+
+const createAccountAndProfessional = new CreateAccountAndProfessional(createAccountUseCase, createProfessionalUseCase);
