6a1ec6ea566d182c3564edb5a627b2fcdfda0b36
jaymeco <jayme.anunciacao@gmail.com>
2021-03-24 13:20:35 -0300

    Adiciona rotas de login de profissionais

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { createAccountPatientController } from './main/CreateAccountAndPatient';
 import { createAccountAndProfessional } from './main/CreateAccountAndProfessional';
 import { loginPatientController } from './main/LoginPatient';
+import { loginProfessionalController } from './main/LoginProfessional';
 const routes = Router();
 
 routes.post('/patients', (request, response)=>{
@@ -14,6 +15,10 @@ routes.post('/professionals', (request, response)=>{
 
 routes.post('/login/patient', (request, response)=>{
   return loginPatientController.handle(request, response);
-})
+});
+
+routes.post('/login/professional', (request, response)=>{
+  return loginProfessionalController.handle(request, response);
+});
 
 export { routes };
