61cc3a885743758dfbf83cdccbd285f4520ef412
jaymeco <jayme.anunciacao@gmail.com>
2021-03-23 22:47:20 -0300

    Adiciona controller de login

---
+++ src/adapters/controllers/LoginPatientController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { LoginPatient } from "../../domain/UseCases/LoginPatient";
+
+export class LoginPatientController {
+  private loginPatientUseCase;
+
+  constructor (loginPatientUseCase: LoginPatient) {
+    this.loginPatientUseCase = loginPatientUseCase;
+  }
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { email, password } = request.body;
+
+      const data = await this.loginPatientUseCase.execute({ email ,password });
+
+      return response.status(200).json(data);
+    } catch (error) {
+      return response.status(400).json({
+        message: error.message || 'Unexpected error!'
+      });
+    }
+  }
+}
