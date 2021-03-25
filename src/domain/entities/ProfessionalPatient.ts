978c38023ba50aca09f47aa4244f57118bc56533
jaymeco <jayme.anunciacao@gmail.com>
2021-03-25 20:01:37 -0300

    Adiciona entidade de ProfessionalPatient

---
+++ src/domain/entities/ProfessionalPatient.ts
@@ -0,0 +1,32 @@
+import { PrimaryColumn, Column, CreateDateColumn, JoinColumn, Entity, ManyToMany } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Patient } from './Patient';
+import { Professional } from './Professional';
+
+export class ProfessionalPatient {
+  @PrimaryColumn()
+  public readonly id: string;
+
+  @Column()
+  public professional_id: string;
+
+  @Column()
+  public patient_id: string;
+
+  @CreateDateColumn()
+  public created_at: Date;
+
+  @ManyToMany(()=> Patient)
+  @JoinColumn({ name: 'patient_id' })
+  public patient: Patient;
+
+  @ManyToMany(()=> Patient)
+  @JoinColumn({ name: 'professional_id' })
+  public professional: Professional;
+
+  constructor () {
+    if(!this.id) {
+      this.id = uuid();
+    }
+  }
+}
