4c2bca18e25bdf6a2fbb2a56b6829a8ee4364db4
jaymeco <jayme.anunciacao@gmail.com>
2021-03-25 21:20:10 -0300

    Adiciona controller de conexão entre pacientes e profissionais

---
+++ src/adapters/controllers/ConectPatientWithProfessionalController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { ConectProfessionalWithPatient } from "../../domain/UseCases/ConectProfessionalWithPatient";
+
+export class ConectPatientWithProfessionalController {
+  private conectPatientWithProssionalUseCase;
+
+  constructor (conectPatientWithProssionalUseCase: ConectProfessionalWithPatient){
+    this.conectPatientWithProssionalUseCase = conectPatientWithProssionalUseCase;
+  }
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { id, email } = request.body;
+
+      const data = await this.conectPatientWithProssionalUseCase.execute({ id, email });
+
+      return response.status(200).json(data);
+    } catch (error) {
+      return response.status(400).json({
+        message: error.message || 'Unexpectd error!'
+      });
+    }
+  }
+}
