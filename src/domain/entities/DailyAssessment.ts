7810983336f627d7e868f97fa8fb7821a9dcd13e
jaymeco <jayme.anunciacao@gmail.com>
2021-04-17 12:32:06 -0300

    Adiciona entidade de avaliação diária

---
+++ src/domain/entities/DailyAssessment.ts
@@ -0,0 +1,27 @@
+export class DailyAssessment {
+  public readonly id: string;
+
+  public patient_id: string;
+
+  public question_01: boolean;
+
+  public question_02: string;
+
+  public question_03: number;
+
+  public question_04: number;
+
+  public question_05: number;
+
+  public question_06: number;
+
+  public question_07: number;
+
+  public question_08: number;
+
+  public question_09: number;
+
+  public question_10: number;
+
+  public question_11: number;
+}
