ea77e5d5f52d4e99c6112d337c0fe59916c45682
jaymeco <jayme.anunciacao@gmail.com>
2021-04-23 21:07:10 -0300

    Adiciona controller do gráfico três

---
+++ src/adapters/GetGraphicThreeValues.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { GetGraphicThreeValues } from "../domain/UseCases/GetGraphicThreeVales";
+
+export class GetGraphicThreeValuesController {
+  private getGraphicThreeValuesUseCase;
+
+  constructor (getGraphicThreeValuesUseCase: GetGraphicThreeValues){
+    this.getGraphicThreeValuesUseCase = getGraphicThreeValuesUseCase;
+  }
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { id } = request.params;
+
+      const dailies = await this.getGraphicThreeValuesUseCase.execute(id);
+
+      return response.status(200).json(dailies);
+    } catch (error) {
+      return response.status(400).json({
+        message: error.message
+      })
+    }
+  }
+}
