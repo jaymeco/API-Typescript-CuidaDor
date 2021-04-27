a52363e0a349ac5d456473a7b786c3ed2990c18f
jaymeco <jayme.anunciacao@gmail.com>
2021-04-26 21:38:50 -0300

    Adiciona repositorio do educação em dor

---
+++ src/infra/repositories/PostgresPainEducationRepository.ts
@@ -0,0 +1,5 @@
+import { EntityRepository, Repository } from "typeorm";
+import { PainEducation } from "../../domain/entities/PainEducation";
+
+EntityRepository(PainEducation)
+export class PostgresPainEducationRepository extends Repository<PainEducation>{}
