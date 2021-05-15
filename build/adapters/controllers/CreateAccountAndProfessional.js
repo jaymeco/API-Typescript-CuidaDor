29082a3d9c57a872170e24be678e94743ef3c5f3
jaymeco <jayme.anunciacao@gmail.com>
2021-05-14 23:15:37 -0300

    Corrigindo erros

--- build/adapters/controllers/CreateAccountAndProfessional.js
+++ build/adapters/controllers/CreateAccountAndProfessional.js
@@ -1,15 +1,4 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
-    return t;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CreateAccountAndProfessional = void 0;
 class CreateAccountAndProfessional {
@@ -19,9 +8,12 @@ class CreateAccountAndProfessional {
     }
     async handle(request, response) {
         try {
-            const _a = request.body, { email, password } = _a, rest = __rest(_a, ["email", "password"]);
+            const { email, password, ...rest } = request.body;
             const account = await this.createAccountUseCase.execute({ email, password });
-            const professional = await this.createProfessionalUseCase.execute(Object.assign({ account_id: account.id }, rest));
+            const professional = await this.createProfessionalUseCase.execute({
+                account_id: account.id,
+                ...rest
+            });
             return response.status(201).json({ account, professional });
         }
         catch (error) {
--- build/adapters/controllers/CreateGoalController.js
+++ build/adapters/controllers/CreateGoalController.js
@@ -1,15 +1,4 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
-    return t;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CreateGoalController = void 0;
 class CreateGoalController {
@@ -18,10 +7,13 @@ class CreateGoalController {
     }
     async handle(request, response) {
         try {
-            const _a = request.body, { content, patient_id, professional_id } = _a, rest = __rest(_a, ["content", "patient_id", "professional_id"]);
-            const goal = await this.createGoalUseCase.execute(Object.assign({ content,
+            const { content, patient_id, professional_id, ...rest } = request.body;
+            const goal = await this.createGoalUseCase.execute({
+                content,
                 patient_id,
-                professional_id }, rest));
+                professional_id,
+                ...rest
+            });
             return response.status(201).json(goal);
         }
         catch (error) {
