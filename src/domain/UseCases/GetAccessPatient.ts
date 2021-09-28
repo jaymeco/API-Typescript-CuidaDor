af2664371dacfbe96fbf0f0faa9f48117fa15242
jaymeco <jayme.anunciacao@gmail.com>
2021-09-27 21:06:28 -0300

    Termina implementaçao da consulta do acesso de paciente.

--- src/domain/UseCases/GetAccessPatient.ts
+++ src/domain/UseCases/GetAccessPatient.ts
@@ -1,15 +1,32 @@
 import { getCustomRepository } from "typeorm";
 import { PostgresDailyAssessmentRespository } from "../../infra/repositories/PostgresDailyAssessment";
+import { AccessPatientResponse } from "../DTOs/GetAccessPatient";
 
 export class GetAccessPatient {
 
-  async execute(id: string): Promise<number> {
+  async execute(id: string, data: string): Promise<AccessPatientResponse> {
     const postgresDailyAssessmentRepository = getCustomRepository(PostgresDailyAssessmentRespository);
-
-    const dailiesAssessments = await postgresDailyAssessmentRepository.count({
-      patient_id: id
+    const dailiesAssessments = await postgresDailyAssessmentRepository.find({
+      where: {
+        patient_id: id,
+      },
     });
 
-    return dailiesAssessments;
+    const avaliacoesFeitas = dailiesAssessments.filter((avalicao) => {
+      const avaliacaoMes = new Date(avalicao.created_at).getMonth() + 1;
+      const avaliacaoAno = new Date(avalicao.created_at).getFullYear();
+
+      const atualMes = new Date(data).getMonth() + 1;
+      const atualAno = new Date(data).getFullYear();
+
+      if (avaliacaoMes === atualMes && avaliacaoAno === atualAno) {
+        return avalicao;
+      }
+    });
+
+    return {
+      diasFeitos: avaliacoesFeitas.length,
+      porcentagem: (avaliacoesFeitas.length * 100) / 30,
+    };
   }
 }
