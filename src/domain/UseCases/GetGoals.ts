c24fe431c0e79e65fe0edb806f36009ae76e87e2
jaymeco <jayme.anunciacao@gmail.com>
2021-10-17 22:38:58 -0300

    Remodela retorno da listagem de metas.

--- src/domain/UseCases/GetGoals.ts
+++ src/domain/UseCases/GetGoals.ts
@@ -3,14 +3,19 @@ import { PostgresGoalRepository } from "../../infra/repositories/PostgresGoalRep
 import { PostgresPatientRepository } from "../../infra/repositories/PostgresPatientRepository";
 import { Goal } from "../entities/Goal";
 
+interface Response {
+  emAndamento: Goal[],
+  concluidos: Goal[],
+  naoConcluidos: Goal[],
+}
 export class GetGoals {
-  async execute(patient_id: string): Promise<Goal[]>{
+  async execute(patient_id: string): Promise<Response> {
     const postgresGoalRepository = getCustomRepository(PostgresGoalRepository);
     const postgresPatientRepository = getCustomRepository(PostgresPatientRepository);
 
     const patientExists = await postgresPatientRepository.findOne({ id: patient_id });
 
-    if(!patientExists){
+    if (!patientExists) {
       throw new Error('This patient does not exists!');
     }
 
@@ -18,6 +23,10 @@ export class GetGoals {
       where: { patient_id: patient_id }
     });
 
-    return goals;
+    return {
+      emAndamento: goals,
+      concluidos: goals,
+      naoConcluidos: goals,
+    };
   }
 }
