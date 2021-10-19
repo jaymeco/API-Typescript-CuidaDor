4beb1dc42935b687ae7faf359a1ca0ff6179c1ac
jaymeco <jayme.anunciacao@gmail.com>
2021-10-18 22:26:22 -0300

    Adiciona controller de consulta do ultimo acesso.

---
+++ src/adapters/controllers/GetLastAccessController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { GetLastAccess } from "../../domain/UseCases/GetLastAccess";
+
+export class GetLastAccessController {
+  private getLastAccessUseCase;
+
+  constructor (getLastAccessUseCase: GetLastAccess){
+    this.getLastAccessUseCase = getLastAccessUseCase;
+  }
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { patient_id } = request.params;
+
+      const lastAccess = this.getLastAccessUseCase.execute(patient_id);
+
+      return response.status(200).json({
+        ultimoAcesso: lastAccess,
+      });
+    } catch (error: any) {
+      return response.status(400).json({
+        message: error.message || 'Unexpected error!'
+      })
+    }
+  }
+}
