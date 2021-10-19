44e10baa333aa7533211a16da5b540f31344740d
jaymeco <jayme.anunciacao@gmail.com>
2021-10-18 21:39:49 -0300

    Remove coluna de runtime e adiciona colunas de data inicio e data final.

--- src/domain/DTOs/CreateGoal.ts
+++ src/domain/DTOs/CreateGoal.ts
@@ -2,6 +2,7 @@ export interface ICreateGoalRequestDTO {
   content: string;
   patient_id: string;
   professional_id: string;
-  frequency_per_week: number,
-  runtime: number
+  frequency_per_week: number;
+  dataInicio: string;
+  dataFinal: string;
 }
