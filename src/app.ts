13956f98338fd7aa65ff7ab9559e44030bd86420
jaymeco <jayme.anunciacao@gmail.com>
2021-11-27 22:13:58 -0300

    refactor(app): Adiciona configuração para o webSocket.

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,23 @@ import 'reflect-metadata';
 import './infra/database/typeorm';
 import express from 'express';
 import { routes } from './routes';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 import cors from 'cors';
 const app = express();
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-export { app };
+const http = createServer(app);
+const io = new Server(http,
+  {
+    cors: {
+      origin: "http://localhost:3000",
+      credentials: true,
+      methods: ["GET", "POST"],
+    }
+  }
+);
+
+export { http, io };
