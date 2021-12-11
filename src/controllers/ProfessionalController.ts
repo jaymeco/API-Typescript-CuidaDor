f508c3f375031b1f340468e4f2330a1eca61b23e
jaymeco <jayme.anunciacao@gmail.com>
2021-12-10 23:56:38 -0300

    feature(controllers/Professional): Adiciona controller de profissional com o método de atualização.

---
+++ src/controllers/ProfessionalController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { Professional } from "../domain/entities/Professional";
+import ProfessionalService from "../services/ProfessionalService";
+
+class ProfessionalController {
+  private service;
+
+  constructor() {
+    this.service = ProfessionalService;
+  }
+
+  public async updateProfessional(request: Request, response: Response) {
+    try {
+      const { professional_id } = request.params;
+      const data = request.body;
+
+      await this.service.updateProfessional(
+        data,
+        professional_id,
+      );
+
+      return response.status(200).json({
+        message: 'Profissional atualizado com sucesso!',
+      });
+    } catch (error: any) {
+      return response.status(400).json({
+        message: error.message,
+      });
+    }
+  }
+}
+
+export default new ProfessionalController();
